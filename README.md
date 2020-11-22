6c6a81e858d45e5cc699daae373426f6d478843b
Megan Ku <mku@olin.edu>
2020-11-22 01:05:52 -0500

    Add questions in README

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ homework assignment.
 The file `linkedlist_test.py` is the test code.
 
 ## Supplemental Questions (To be asked @ Check-in!)
+
+How does a doubly-linked list compare to a regular Python list? Name some similarities and differences.
+
+How did you make sure that the DLL was still functioning even as you removed a node? (Talk through the assignment process).
+
+How did you keep track of the length of the DLL?
--- linkedlist_test.py
+++ linkedlist_test.py
@@ -5,21 +5,31 @@ Test script for linked list hw
 from ku_submission import Node, DLL
 
 def test_DLL_Class():
+    '''
+    Create a DLL with the following elements (in order)
+    [1 <-> 2 <-> 3 <-> 4]
+    and tests the following:
+    Attributes: head, tail, prev, next
+    Methods: length(), index(), push(), insert(), delete(),
+    multiply_all_pairs()
+    '''
 
-    # Create a DLL with the following elements (in order)
-    # 1, 2, 3, 4
-
+    # Create empty list
     test_DLL = DLL()
     assert test_DLL.length() == 0
     assert test_DLL.multiply_all_pairs() == 0
 
+    # DLL = 4
     test_DLL.push(4)
     assert test_DLL.length() == 1
     assert test_DLL.head.val == 4
     assert test_DLL.tail.val == 4
 
+    # DLL = 2 <-> 4
     test_DLL.push(2)
     head_node = test_DLL.head
+
+    # DLL = 2 <-> 3 <-> 4
     test_DLL.insert(3, head_node)
     assert test_DLL.length() == 3
     assert test_DLL.multiply_all_pairs() == 26
@@ -27,6 +37,7 @@ def test_DLL_Class():
     assert head_node.next == second_node
     assert second_node.prev == head_node
 
+    # DLL = 1 <-> 2 <-> 3 <-> 4
     test_DLL.push(1)
     assert test_DLL.length() == 4
     assert test_DLL.multiply_all_pairs() == 35
@@ -36,13 +47,16 @@ def test_DLL_Class():
     assert test_DLL.index(2).val == 3
     assert test_DLL.index(3).val == 4
 
+    # DLL = 1 <-> 2 <-> 4
     test_DLL.delete(test_DLL.tail.prev)
     assert test_DLL.tail.prev == test_DLL.head.next
     assert test_DLL.length() == 3
 
+    # DLL = 2 <-> 4
     test_DLL.delete(test_DLL.head)
-    assert test_DLL.head.val == 2
+    assert test_DLL.tail.val == 4
 
+    # DLL = 2
     test_DLL.delete(test_DLL.tail)
     assert test_DLL.head == test_DLL.tail
     assert test_DLL.head.val == 2
