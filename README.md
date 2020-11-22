74e7210ecb6a59153f66f40ad7624b23676942ab
Megan Ku <mku@olin.edu>
2020-11-22 00:55:41 -0500

    complete test file

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Sample HW for DSA
 ## Contents
-The code `linkedlist_starter.py` is the template that Alice provided for the
+The file `linkedlist_starter.py` is the template that Alice (S'20) provided for the
 linked list assignment.
+The file `ku_submission.py` is code from my personal submission from the
+homework assignment.
+The file `linkedlist_test.py` is the test code.
+
+## Supplemental Questions (To be asked @ Check-in!)
--- linkedlist_test.py
+++ linkedlist_test.py
@@ -4,5 +4,45 @@ Test script for linked list hw
 
 from ku_submission import Node, DLL
 
-def test_DLL():
-    
+def test_DLL_Class():
+
+    # Create a DLL with the following elements (in order)
+    # 1, 2, 3, 4
+
+    test_DLL = DLL()
+    assert test_DLL.length() == 0
+    assert test_DLL.multiply_all_pairs() == 0
+
+    test_DLL.push(4)
+    assert test_DLL.length() == 1
+    assert test_DLL.head.val == 4
+    assert test_DLL.tail.val == 4
+
+    test_DLL.push(2)
+    head_node = test_DLL.head
+    test_DLL.insert(3, head_node)
+    assert test_DLL.length() == 3
+    assert test_DLL.multiply_all_pairs() == 26
+    second_node = test_DLL.head.next
+    assert head_node.next == second_node
+    assert second_node.prev == head_node
+
+    test_DLL.push(1)
+    assert test_DLL.length() == 4
+    assert test_DLL.multiply_all_pairs() == 35
+    assert test_DLL.tail.val == 4
+    assert test_DLL.index(0).val == 1
+    assert test_DLL.index(1).val == 2
+    assert test_DLL.index(2).val == 3
+    assert test_DLL.index(3).val == 4
+
+    test_DLL.delete(test_DLL.tail.prev)
+    assert test_DLL.tail.prev == test_DLL.head.next
+    assert test_DLL.length() == 3
+
+    test_DLL.delete(test_DLL.head)
+    assert test_DLL.head.val == 2
+
+    test_DLL.delete(test_DLL.tail)
+    assert test_DLL.head == test_DLL.tail
+    assert test_DLL.head.val == 2
